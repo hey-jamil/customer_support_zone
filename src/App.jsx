482689213eb9936dc3017f17db47b00b80865a91
Tanvir Hasan Jamil <tanvirjamil62@gmail.com>
2025-11-13 02:06:21 +0600

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
+import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
+    const [inProgress, setInProgress] = useState(0)
+  const [resolved, setResolved] = useState([])
+  const [customers, setCustomers] = useState([])
 
   return (
     <>
       <Navbar></Navbar>
+      <Banner inProgress={inProgress} resolved={resolved}></Banner>
     </>
   )
 }
